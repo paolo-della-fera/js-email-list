d9194e1c7a713fb99bf411f10e4b546df2a416ef
paolo-della-fera <paolodf2005@gmail.com>
2026-02-23 16:51:50 +0100

    add function and click event listener to button to generate new emails

--- main.js
+++ main.js
@@ -3,26 +3,40 @@ const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
 // collego i nodi del DOM a js
 const emailListEl = document.getElementById('email-list')
+const newGeneration = document.getElementById('btn')
 
-
-// creo un ciclo for per generarmi 10 email differenti 
-for (let i = 0; i < 10; i++) {
+// aggiungo tutto all'interno di una funzione 
+function newEmail() {
     
-    // recupero il dato dell'API che mi serve 
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(email => {
-        
-            // mi servo il risultato in una variabile e lo stampo in console 
-            const userEmail = email.response
-            console.log(userEmail)
-
-            // creo il list item
-            const li = document.createElement('li');
-            // inserisco nel list item le email 
-            li.textContent = email.response;
-            // aggiungo i list item dentro la lista creata precedentemente
-            emailListEl.appendChild(li)
-        })
+    emailListEl.innerHTML = '';
     
+    // creo un ciclo for per generarmi 10 email differenti 
+    for (let i = 0; i < 10; i++) {
+
+        // recupero il dato dell'API che mi serve 
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(email => {
+
+                // mi servo il risultato in una variabile e lo stampo in console 
+                const userEmail = email.response
+                console.log(userEmail)
+
+                // creo il list item
+                const li = document.createElement('li');
+                // inserisco nel list item le email 
+                li.textContent = email.response;
+                // aggiungo i list item dentro la lista creata precedentemente
+                emailListEl.appendChild(li)
+            })
+
+    }
+
 }
+
+newEmail()
+// assegno al bottone l'evento in modo da generare nuove email al click
+newGeneration.addEventListener('click', function () {
+    newEmail()
+})
+
