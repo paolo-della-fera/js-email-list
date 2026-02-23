e4ef64c354dbbbc00f5c9e49bd3a8c1ce59926f5
paolo-della-fera <paolodf2005@gmail.com>
2026-02-23 16:11:01 +0100

    add generated emails to list items and append them to the list

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // assegno a una variabile il mio API
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
 
+// collego i nodi del DOM a js
+const emailListEl = document.getElementById('email-list')
+
 
 // creo un ciclo for per generarmi 10 email differenti 
 for (let i = 0; i < 10; i++) {
@@ -13,6 +16,13 @@ for (let i = 0; i < 10; i++) {
             // mi servo il risultato in una variabile e lo stampo in console 
             const userEmail = email.response
             console.log(userEmail)
+
+            // creo il list item
+            const li = document.createElement('li');
+            // inserisco nel list item le email 
+            li.textContent = email.response;
+            // aggiungo i list item dentro la lista creata precedentemente
+            emailListEl.appendChild(li)
         })
     
 }
